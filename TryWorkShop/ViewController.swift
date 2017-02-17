feeaf257faa1574d146dbeaab77f24205998b18c
lotus <lotusho12312@gmail.com>
2017-02-17 11:18:43 +0800

    Initial Commit

---
+++ TryWorkShop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TryWorkShop
+//
+//  Created by FJ on 17/2/2017.
+//  Copyright © 2017年 1.0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
