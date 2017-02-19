1864ae35ff62d0962e0085987d6e9486c27dacd1
kwleungaj <kl970808@gmail.com>
2017-02-19 22:09:59 +0800

    basic UI done
    Navigation and table view done

---
+++ TryWorkShop/EventDetailsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  EventDetailsViewController.swift
+//  TryWorkShop
+//
+//  Created by Ka Wing Leung on 19/2/2017.
+//  Copyright © 2017年 1.0. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailsViewController: UIViewController {
+
+    @IBOutlet weak var eventDescription: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        eventDescription.text = "Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple.Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. Development of Swift started on July 2010 by Chris Lattner, with the eventual collaboration of many other programmers at Apple. "
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TryWorkShop/EventListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventListTableViewCell.swift
+//  TryWorkShop
+//
+//  Created by Ka Wing Leung on 19/2/2017.
+//  Copyright © 2017年 1.0. All rights reserved.
+//
+
+import UIKit
+
+class EventListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
